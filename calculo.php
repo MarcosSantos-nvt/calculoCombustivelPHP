ded87e303253f126f8d834969aea13e068ff5e25
Marcos R Santos <marcos.sbnf@gmail.com>
2020-03-14 12:00:52 -0300

    adicionado arquivo html para exibição dos calculos

--- calculo.php
+++ calculo.php
@@ -1,5 +1,7 @@
 <?php
 
+    $mensagemResultado = "";
+
     if(isset($_POST['distancia']) && isset($_POST['autonomia'])) {
         if ($_POST['distancia'] > 0 && $_POST['autonomia'] > 0){            
                 $distancia = $_POST['distancia'];
@@ -13,9 +15,9 @@
                 $calculoAcool = ($distancia / $autonomia) * $precoAlcool;
                 $calculoAcool= number_format($calculoAcool, 2, ',', '.');
 
-                echo "OS GASTOS COM GASOLINA SERÃO: R$" .$calculoGasolina;
+                $mensagemResultado.= "<p>OS GASTOS COM GASOLINA SERÃO: R$" .$calculoGasolina."</p>";
                 echo "</br>";
-                echo "OS GASTOS COM ALCOOL SERÃO: R$" .$calculoAcool;
+                $mensagemResultado.= "<p>OS GASTOS COM ALCOOL SERÃO: R$" .$calculoAcool."</p>";
         } else{
             echo "OS FORMULARIOS ANTERIORES POSSUEM VALORES ZERADOS OU NEGATIVOS O QUE IMPEDE O CALCULO!";
         }
@@ -23,3 +25,28 @@
     } else{
         echo "OS FORMULARIOS ANTERIORES DEVEM SER PREENCHIDOS COM VALORES VALIDOS PARA O CALCULO!";
     }
+
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Calculadora de Combustível</title>
+    </head>
+    <body>
+        <main>
+            <div class="painel">
+                <h1>Resultado dos calculos:</h1>
+            </div>
+            <div class="conteudo-painel">
+                <?php
+                    echo $mensagemResultado;
+                ?>
+                <a class="botao" href="index.php">Voltar</a>
+            </div>
+        </main>
+        
+    </body>
+    </html>
